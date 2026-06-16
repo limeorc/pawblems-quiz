405488cc900a8f8ba80adf78ef35a3b64df8cce8
limeorc <iamriqing@gmail.com>
2026-06-16 19:55:06 +0800

    Update config.js
    更新分組名稱

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ window.QUIZ_CONFIG = {
   googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbwWABH-3VHa-ezVU71B96t_TQrQsmP1c8RJRVeM2imTIBEnleBVK0WahGTu2MEN_24ABQ/exec',
   tieBreaker: 'lastAnswer',
   types: {
-    brain: '腦洞警探',
-    aesthetic: '美感鑑識員',
-    structure: '結構重案組長'
+    brain: '重案偵查戰略分析組',
+    aesthetic: '視覺鑑識修復組',
+    structure: '科技虛擬重建組'
   }
 };
